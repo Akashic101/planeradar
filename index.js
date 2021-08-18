09f5327cbcb9def9f682a242f75bc6552568851d
David Moll <david.leander.moll@gmail.com>
2021-08-18 16:25:38 +0200

    Added writing to json-file

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const radar = require('flightradar24-client/lib/radar') //https://www.npmjs.com/package/flightradar24-client#radarnorth-west-south-east
 const flight = require('flightradar24-client/lib/flight') //https://www.npmjs.com/package/flightradar24-client#flightid
 var cron = require('node-cron'); //https://www.npmjs.com/package/node-cron
+const fs = require('fs');
+
 
 cron.schedule('* * * * *', () => { //(minute, hour, day of month, month, day of week)
 	radar(52.270, 8.168, 51.591, 8.996) //(north(Latitude), west(Longitude), south(Latitude), east(Longitude))
@@ -8,22 +10,45 @@ cron.schedule('* * * * *', () => { //(minute, hour, day of month, month, day of 
 			if (!radar.length) {
 				console.log("There is no plane above your house")
 			} else {
-				console.log(`There are ${radar.length} planes above your house! \n`)
+				console.log(`There are ${radar.length} planes above your house!`)
+
+				//With the help of: https://stackoverflow.com/questions/36856232/write-add-data-in-json-file-using-node-js
+
+				//Create a JavaScript object with the table array in it
+				var flightsList = { 
+					flights: []
+				};
+
 				radar.forEach(plane => {
 					flight(`${plane.id}`)
 						.then((planeInfo) => {
-							console.log(`Plane info:
-							Plane: ${planeInfo.airline} ${planeInfo.model}
-							Callsign: ${plane.callsign}
-							Flight: ${plane.flight}
-							Speed: ${plane.speed} knots (${(plane.speed*1.852).toFixed(2)}kp/h)
-							Altitude: ${plane.altitude} feet (${(plane.altitude*0.3048).toFixed(2)}m)
-							Flight info:
-							Origin: ${planeInfo.origin.name} (${planeInfo.origin.id})
-							Destination: ${planeInfo.destination.name} (${planeInfo.destination.id}) \n`);
+							flightsList.flights.push({ //Add some data to it
+								id: plane.id,
+								callsign: plane.callsign,
+								flight: plane.flight,
+								Altitude: plane.altitude,
+								speed: plane.speed,
+								origin: {
+									id: planeInfo.origin.id,
+									name: planeInfo.origin.name,
+									country: planeInfo.origin.country
+								},
+								destination: {
+									id: planeInfo.destination.id,
+									name: planeInfo.destination.name,
+									country: planeInfo.destination.country
+								}
+							});
+
+							try {
+								var data = JSON.stringify(flightsList); //Convert it from an object to a string with JSON.stringify
+
+								fs.writeFileSync('./planeflight.json', data, 'utf8'); //Use fs to write the file to disk (filename, data, fileformat)
+							} catch (err) {
+								console.log(`Error writing file: ${err}`);
+							}
 						})
 				})
-				console.log(``)
 			}
 		});
 });
